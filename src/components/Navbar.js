a9818bbf7fd7cba1a762bd7a936c75be03e22628
Mario Kanpisa <mariokanpisa1@gmail.com>
2025-07-20 02:31:03 +0700

    Fix build: pakai Link di Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <nav className="w-full bg-neutral-900 text-white p-4 flex justify-between items-center">
       <h1 className="font-bold text-xl">My Berita Olahraga</h1>
       <ul className="flex gap-4">
-        <li><a href="/" className="hover:text-white">Home</a></li>
-        <li><a href="/berita" className="hover:text-white">Berita</a></li>
-        <li><a href="/jadwal" className="hover:text-white">Jadwal</a></li>
+        <li><Link href="/" className="hover:text-white">Home</Link></li>
+        <li><Link href="/berita" className="hover:text-white">Berita</Link></li>
+        <li><Link href="/jadwal" className="hover:text-white">Jadwal</Link></li>
       </ul>
     </nav>
   );
