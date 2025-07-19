7cc38e076305bd80b36f744887ce37bd47dc5e41
Mario Kanpisa <mariokanpisa1@gmail.com>
2025-07-20 02:04:20 +0700

    Fix Sidebar.js file name

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,13 +1,12 @@
 export default function Sidebar() {
   return (
-    <aside className="bg-gray-900 text-white p-4 w-full md:w-64 rounded-lg">
-      <h2 className="text-lg font-semibold mb-4">Kategori Berita</h2>
+    <aside className="bg-gray-800 text-white p-4 rounded-lg">
+      <h2 className="font-bold text-lg mb-4">Kategori</h2>
       <ul className="space-y-2">
-        <li><a href="#" className="hover:text-gray-400">Liga Inggris</a></li>
-        <li><a href="#" className="hover:text-gray-400">Liga Spanyol</a></li>
-        <li><a href="#" className="hover:text-gray-400">Liga Italia</a></li>
-        <li><a href="#" className="hover:text-gray-400">Tim Nasional</a></li>
-        <li><a href="#" className="hover:text-gray-400">Transfer Pemain</a></li>
+        <li><a href="#" className="hover:underline">Sepak Bola</a></li>
+        <li><a href="#" className="hover:underline">Basket</a></li>
+        <li><a href="#" className="hover:underline">MotoGP</a></li>
+        <li><a href="#" className="hover:underline">F1</a></li>
       </ul>
     </aside>
   );
